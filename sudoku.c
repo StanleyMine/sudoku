a4c488feec6e43be35ccc3e61daa291f36ff0ad4
Stanley <staanleyminervini@gmail.com>
2021-02-09 06:26:19 -0500

    Add Comments

--- sudoku.c
+++ sudoku.c
@@ -61,10 +61,10 @@ int solvePuzzle()
         for (col = 0; col < 9; col++)
         {
             if (puzzle[row][col] != 0)
-            {
+            { // check if value is already in the row, and if so, return false
                 if (contains[puzzle[row][col] - 1])
                     return FALSE;
-            }
+            } // else, add the value to the int array
             contains[puzzle[row][col] - 1] = TRUE;
         }
     }
@@ -80,14 +80,14 @@ int solvePuzzle()
         for (row = 0; row < 9; row++)
         {
             if (puzzle[row][col] != 0)
-            {
+            { // check if value is in col
                 if (contains[puzzle[row][col] - 1] == 1)
                     return FALSE;
-            }
+            } // assign value
             contains[puzzle[row][col] - 1] = TRUE;
         }
     }
-
+    // add by 3 because of the grids of 9
     for (subRow = 0; subRow < 9; subRow += 3)
     {
         for (subCol = 0; subCol < 9; subCol += 3)
@@ -99,14 +99,14 @@ int solvePuzzle()
             }
 
             for (row = 0; row < 3; row++)
-            {
+            { // this is checking the grid
                 for (col = 0; col < 3; col++)
-                {
+                { // this is utilizing outer for loops
                     r = row + subRow;
                     c = col + subCol;
 
                     if (puzzle[r][c] != 0)
-                    {
+                    { // filled in values have been skipped, so this is only checking values that have been assigned
                         if (contains[puzzle[r][c] - 1])
                             return 0;
                         contains[puzzle[r][c] - 1] = TRUE;
@@ -121,32 +121,40 @@ int solvePuzzle()
 
 int backtrack(int r, int c)
 {
+    // check if valid puzzle
     if (!solvePuzzle())
     {
         return FALSE;
     }
-
+    // reached end of col, loop to next row
     if (c == 9)
     {
         return backtrack(r + 1, 0);
     }
+    // if at end of rows, must be at the end of the puzzle
     if (r == 9)
     {
         return TRUE;
     }
+    // skipping over filled in values
     if (puzzle[r][c] != 0)
     {
         return backtrack(r, c + 1);
     }
-
+    // trying to use all possible values
     int attempt;
     for (attempt = 1; attempt <= 9; attempt++)
     {
         puzzle[r][c] = attempt;
+        // move onto next square and see if this attempt is legit
+        // as soon as a fail is hit, need to revert back
+        // this is where the call stack goes insane, it's going to attempt to fill
+        // all of the empties until it fails or hits end of puzzle
         if (backtrack(r, c + 1) == 1)
         {
             return TRUE;
         }
+        // set value back because it did not work
         puzzle[r][c] = 0;
     }
     return FALSE;
