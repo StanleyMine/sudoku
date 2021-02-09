8e5f9398f9f1667012976292bab39d27c67e0511
Stanley <staanleyminervini@gmail.com>
2021-02-09 05:09:36 -0500

    Pushing helper functions

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int solvePuzzle(int puzzle[9][9]);
+int isPuzzleEmpty(int puzzle[9][9]);
+int isNumberRepeat(int puzzle[9][9], int num, int col, int row);
+int checkBox(int puzzle[9][9], int row, int col, int num);
+
 int main()
 {
     // assume given grid of 9x9 we need to first solve it
@@ -21,16 +26,54 @@ int main()
         }
     }
     fclose(fp);
-    //printf("\n");
 
+    // reading in puzzle correctly from file, now to implement solving technique
+
+    return 1;
+}
+
+// Using backtrack algo to solve puzzle
+int solvePuzzle(int puzzle[9][9])
+{
+}
+
+// Function to check if puzzle is empty, and returns 1 when puzzle has empty squares
+int isPuzzleEmpty(int puzzle[9][9])
+{
     for (int i = 0; i < 9; i++)
     {
         for (int j = 0; j < 9; j++)
         {
-            printf("%d ", puzzle[i][j]);
+            if (puzzle[i][j] == 0)
+            {
+                return 1;
+            }
         }
-        printf("\n");
     }
+    return 0;
+}
 
-    return 1;
+// Function to check if number is valid, returns 1 if number is not valid
+int isNumberRepeat(int puzzle[9][9], int num, int col, int row)
+{
+    for (int i = 0; i < 9; i++)
+    { // checking row and col
+        if (num == puzzle[row][i] || num == puzzle[i][col])
+            return 1;
+    }
+    return 0;
+}
+
+// Checking 3x3 box for matching number, per sudoku rules
+int checkBox(int puzzle[9][9], int row, int col, int num)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (puzzle[row + i][col + j] == num)
+                return 1;
+        }
+    }
+    return 0;
 }
